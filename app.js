7b7ceeb502a50fa94986fca26ca30caaac0cce30
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 14:23:32 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // write code here
+บรรทัดที่มี * ให้ผลลัพธ์เป็นอะไร เพราะอะไร
+
+const user = {
+  email: 'cc@gmail.com',
+  isActive: true
+};
+
+user.isActive = false;
+console.log(user); // *
+user = {};
+console.log(user); // **
+
+
+บรรทัดที่มี * ให้ผลลัพธ์เป็นอะไร เพราะอะไร
+  * ข้างบน isActive false 
+  const user = {
+  email: 'cc@gmail.com',
+  isActive: false
+
+** ข้างล่าง จะแสดง isActive true
+const user = {
+  email: 'cc@gmail.com',
+  isActive: true
